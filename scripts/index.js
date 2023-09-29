e35b8d696a8e3dcb5e9d9a87f67aafc22ac853a2
Jason Claeys <jdclaeys@outlook.com>
2023-09-28 20:46:44 -0500

    extract build table function and import as a module

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 const fileInput = document.querySelector('#file-input')
 const results = document.querySelector('#results-section')
+import { buildTable } from './table.mjs'
 
 fileInput.addEventListener('change', async e => {
     const [file] = fileInput.files
@@ -11,7 +12,7 @@ fileInput.addEventListener('change', async e => {
             const worksheet = workbook.Sheets[workbook.SheetNames[0]]
             console.log(worksheet)
             
-            buildTable(worksheet)
+            buildTable(worksheet, results)
 
             // const table = XLSX.utils.sheet_to_html(worksheet)
             // results.innerHTML = table
@@ -20,82 +21,3 @@ fileInput.addEventListener('change', async e => {
         reader.readAsArrayBuffer(file)
     }
 })
-
-const buildTable = ws => {
-    let output = ''
-
-    if (Object.keys(ws).length) {
-        output += `
-                <table>
-            `
-
-        if (ws.A1) {
-            output += `
-                <thead>
-                    <tr>
-                        <th colspan='2'>${ws.A1.w}</th>
-                    </tr>
-                </thead>
-            `
-        }
-
-        output += '<tbody>'
-
-        let arr = []
-
-        for (let prop in ws) {
-            arr.push({ prop, value: ws[prop].w })
-        }
-
-        arr.sort((a, b) => {
-            if (a < b) {
-                return -1
-            }
-
-            if (a > b) {
-                return 1
-            }
-
-            return 0
-        })
-
-        console.log(arr)
-
-        arr.forEach(obj => {
-            if (obj.prop === '!ref') {
-                output += ''
-            }
-
-            if (obj.prop === '!margins') {
-                output += ''
-            }
-
-            if (obj.prop.includes('A') && obj.prop !== 'A1') {
-                output += `
-                    <tr>
-                        <td>${obj.value}</td>
-                `
-            }
-
-            if (arr[arr.indexOf(obj) + 1] && arr[arr.indexOf(obj) + 1].prop.includes('A') && obj.prop !== 'A1' && obj.prop !== '!ref' && obj.prop !== '!margins') {
-                output += `
-                        <td>${obj.value}</td>
-                    </tr>
-                `
-            }
-
-            if (!obj.prop.includes('A') && arr[arr.indexOf(obj) + 1] && !arr[arr.indexOf(obj) + 1].prop.includes('A') && obj.prop !== '!ref' && obj.prop !== '!margins') {
-                output += `
-                    <td>${obj.value}</td>
-                `
-            }
-        })
-
-        output += `
-                </tbody>
-            </table>
-        `
-    }
-
-    results.innerHTML = output
-}
---
+++ scripts/table.mjs
@@ -0,0 +1,80 @@
+const buildTable = (ws, results) => {
+    let output = ''
+
+    if (Object.keys(ws).length) {
+        output += `
+                <table>
+            `
+
+        if (ws.A1) {
+            output += `
+                <thead>
+                    <tr>
+                        <th colspan='2'>${ws.A1.w}</th>
+                    </tr>
+                </thead>
+            `
+        }
+
+        output += '<tbody>'
+
+        let arr = []
+
+        for (let prop in ws) {
+            arr.push({ prop, value: ws[prop].w })
+        }
+
+        arr.sort((a, b) => {
+            if (a < b) {
+                return -1
+            }
+
+            if (a > b) {
+                return 1
+            }
+
+            return 0
+        })
+
+        console.log(arr)
+
+        arr.forEach(obj => {
+            if (obj.prop === '!ref') {
+                output += ''
+            }
+
+            if (obj.prop === '!margins') {
+                output += ''
+            }
+
+            if (obj.prop.includes('A') && obj.prop !== 'A1') {
+                output += `
+                    <tr>
+                        <td>${obj.value}</td>
+                `
+            }
+
+            if (arr[arr.indexOf(obj) + 1] && arr[arr.indexOf(obj) + 1].prop.includes('A') && obj.prop !== 'A1' && obj.prop !== '!ref' && obj.prop !== '!margins') {
+                output += `
+                        <td>${obj.value}</td>
+                    </tr>
+                `
+            }
+
+            if (!obj.prop.includes('A') && arr[arr.indexOf(obj) + 1] && !arr[arr.indexOf(obj) + 1].prop.includes('A') && obj.prop !== '!ref' && obj.prop !== '!margins') {
+                output += `
+                    <td>${obj.value}</td>
+                `
+            }
+        })
+
+        output += `
+                </tbody>
+            </table>
+        `
+    }
+
+    results.innerHTML = output
+}
+
+export { buildTable }
